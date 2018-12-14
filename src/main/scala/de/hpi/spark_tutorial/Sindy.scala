94323824eee5a2e7930dd956b751adbc85c2e27b
Alexander Löser <alexander.loeser@student.hpi.uni-potsdam.de>
2018-12-14 16:12:28 +0100

    bla

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,11 +1,33 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object Sindy {
 
+
+
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
-    // TODO
+    var dataframes : List[DataFrame] = List()
+
+    for (input <- inputs) {
+      dataframes = dataframes :+ (spark.read
+        .option("header", "true")
+        .option("delimiter", ";")
+        .option("quote", "\"")
+        .csv(input).toDF)
+    }
+    import spark.implicits._
+    var cells: DataFrame = spark.createDataFrame(,List(("value", StringType, true), ("columnname", StringType, true)))
+    for (dataframe <- dataframes) {
+      val columns = dataframe.columns
+      dataframe.foreach(row => {
+
+        for (index <- row.toSeq.indices) {
+          spark.createDataset(List()).
+        }
+      })
+    }
   }
 }
