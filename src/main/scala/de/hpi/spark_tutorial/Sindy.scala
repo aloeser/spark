d478bb98859f1153a8a6a61df75ceeb70a680151
Alexander Löser <alexander.loeser@student.hpi.uni-potsdam.de>
2018-12-20 16:10:22 +0100

    final changes

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,14 +1,9 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.types.StringType
-import org.apache.spark.sql.functions.explode
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.sql.SparkSession
+
 
 object Sindy {
-
-
-
   def discoverINDs(inputs: List[String], spark: SparkSession, cores: Int): Unit = {
     import spark.implicits._
 
@@ -18,9 +13,8 @@ object Sindy {
       .option("quote", "\"")
       .csv(file).toDF)
 
-
     val zipped = dataframes.map(df => {
-      val headers = df.columns.map(s => List(s));
+      val headers = df.columns//.map(s => List(s));
       df.flatMap(row => {
         row.toSeq.asInstanceOf[Seq[String]].zip(headers)
       })
@@ -28,22 +22,20 @@ object Sindy {
 
     val cells = zipped.reduce(_ union _)
 
-    val cache_preaggregated = cells.rdd.reduceByKey(_ union _)
+    val cache_preaggregated = cells.rdd.groupByKey().mapValues(_.toSet)
 
-    val global_partitioning = cache_preaggregated.repartition(2*cores) // TODO
+    val global_partitioning = cache_preaggregated.repartition(2*cores)
 
-    val attribute_sets = global_partitioning.reduceByKey(_ union _).map(_._2)
+    val attribute_sets = global_partitioning.reduceByKey(_ union _).map(_._2).distinct
 
-    val inclusion_lists = attribute_sets.flatMap(list =>  {list.map(word => Tuple2(word, list.filter(_ != word)))})
 
-    val aggregates = inclusion_lists.reduceByKey(_ intersect _)
+    val inclusion_lists = attribute_sets.flatMap(set => {set.map(word => Tuple2(word, set - word))})
 
+    val aggregates = inclusion_lists.reduceByKey(_ intersect _).filter(_._2.size > 0)
     val output_lines = aggregates.map(aggregate => {aggregate._1 + " < " + aggregate._2.mkString(", ")})
-    output_lines.foreach(print(_))
+    output_lines.collect().sorted.foreach(println(_))
 
-    val ind = aggregates.flatMap(aggregate => { val subset = aggregate._1; aggregate._2.map(Tuple2(subset, _))}).distinct
-
-    ind.toDF.show(50)
+    //val ind = aggregates.flatMap(aggregate => { val subset = aggregate._1; aggregate._2.map(Tuple2(subset, _))}).distinct
   }
 }
 
